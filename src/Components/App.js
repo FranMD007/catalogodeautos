2e409ccf7454bb2ccab7dd0a5ac062252632bbb0
FranMD007 <fran.2305@utp.edu.co>
2021-11-29 21:19:48 -0500

    commit media noche xd

--- src/Components/App.js
+++ src/Components/App.js
@@ -15,7 +15,7 @@ export default class App extends React.Component{
     constructor(props){
         super(props);
         this.state= {
-            logged: true,
+            logged: false,
             username: "",
         };
     }
