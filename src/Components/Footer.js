b003ba0e7d393c7e6fee8b6810f72ced3b68b367
FranMD007 <fran.2305@utp.edu.co>
2021-12-03 19:01:00 -0500

    Imágenesfooter

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,8 +9,19 @@ export default class Footer extends React.Component {
       <div className="footer">
         <div class="container">
           <div class="navbar-nav flex-row flex-wrap ms-md-auto">
-            <div class="col text-light align-middle">Brand</div>
-            <div class="col text-danger align-middle">Powered by </div>
+            <div class="col text-light align-middle">Brand 
+            </div>
+            <div class="col text-danger align-middle">Powered by 
+            <img src='Lamborghini.png' width='30' height='30' alt='' />
+            <img src='Bugatti.png' width='50' height='30' alt='' />
+            <img src='Chevrolet.png' width='40' height='30' alt='' />
+            <img src='Bmw.png' width='30' height='30' alt='' />
+            <img src='AlfaRomeo.png' width='30' height='30' alt='' />
+            <img src='Ferrari.png' width='30' height='30' alt='' />
+            <img src='Porsche.png' width='30' height='30' alt='' />
+            <img src='Ford.png' width='50' height='30' alt='' />
+            <img src='Toyota.png' width='50' height='30' alt='' />
+            </div>
             <div class="col justify-content-end">
               <nav>
                 <ul class="pagination justify-content-end">
